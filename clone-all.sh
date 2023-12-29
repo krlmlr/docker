68d7d3e928944af542b3c6047868cfa2018cea43
Kirill Müller <kirill@cynkra.com>
2023-12-29 04:26:06 +0100

    Fix

--- clone-all.sh
+++ clone-all.sh
@@ -3,5 +3,5 @@
 repositories="krlmlr/rigraph-san krlmlr/rig-centos7 krlmlr/r-minimal cynkra/rig-ubuntu-igraph cynkra/rig-ubuntu cynkra/rig-ubuntu-duckdb cynkra/dm r-dbi/RPostgres"
 
 for repo in $repositories; do
-  gh repo clone $repo repos
+  gh repo clone $repo repos/$repo
 done
