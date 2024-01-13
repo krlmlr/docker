6956a8e7ea0caea6714269d395dc504aa22083c7
Kirill Müller <kirill@cynkra.com>
2024-01-13 17:29:55 +0100

    Only clone if missing

--- clone-all.sh
+++ clone-all.sh
@@ -5,5 +5,8 @@ set -e
 cd $(dirname $0)
 
 for repo in $(cat repos.txt); do
-  gh repo clone $repo repos/$repo
+  if ! [ -d repos/$repo ]; then
+    echo "Cloning $repo"
+    gh repo clone $repo repos/$repo
+  fi
 done
